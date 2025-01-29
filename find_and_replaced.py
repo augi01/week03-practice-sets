44286424bca87401f113041c100410566672f0df
Augi01 <augifrancodeguzman01@gmail.com>
2025-01-29 15:15:47 +0800

    Update find_and_replaced.py

--- find_and_replaced.py
+++ find_and_replaced.py
@@ -1,1 +1,10 @@
+word_to_replace = "old"
+replacement_word = "new"
+with open('file.txt', 'r') as file:
+    content = file.read()
+
+content = content.replace(word_to_replace, replacement_word)
+
+with open('file.txt', 'w') as file:
+    file.write(content)
 
