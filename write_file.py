7e71fdb7e126cdf75e998cc68660abee70b943fc
Augi01 <augifrancodeguzman01@gmail.com>
2025-01-29 15:12:33 +0800

    Update write_file.py
    need pla hiwalay

--- write_file.py
+++ write_file.py
@@ -13,13 +13,3 @@ with open('source.txt', 'r') as src_file:
 with open('destination.txt', 'w') as dest_file:
     dest_file.write(content)
 
-# 4. Write a list of strings to it, each string into a new line
-strings = ["apple", "banana", "cherry"]
-with open('strings.txt', 'w') as file:
-    for string in strings:
-        file.write(string + "\n")
-
-# 5. Write numbers from 1 to 10 to a file, each on a new line
-with open('numbers.txt', 'w') as file:
-    for i in range(1, 11):
-        file.write(str(i) + "\n")
